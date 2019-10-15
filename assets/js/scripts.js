43edc541a1e3fd73448a5fb85064ff71f2208394
Michelle Quinn <michelle.n.quinn@gmail.com>
2019-10-15 16:17:46 -0500

    styling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,10 +3,10 @@ var dateString = moment().format("dddd, MMMM Do YYYY, h:mm a");
 $("#currentDay").html(dateString);
 
 // //create html timeblocks with for loop starting at 9 am until 5pm//
-var plannedHours = [];
+var scheduledHours = [];
 
 for (var hour = 9; hour < 18; hour++) {
-    plannedHours.push(moment({
+    scheduledHours.push(moment({
         hour
     }).format('h  a'));
     $('.container').append(`<div class="row time-block"> 
@@ -18,16 +18,15 @@ for (var hour = 9; hour < 18; hour++) {
       
        <!--//user input text area//-->
            <div class="col-sm col-md-8 d-flex description"> 
-             <textArea class=textArea></textarea> 
+             <textarea class=textArea></textarea> 
            </div> 
       
        <!--//bootstrap saveBtn//-->
-           <div class="col-sm col-md-2 saveBtn"> 
-             <i class="far fa-save fa-2x"></i> 
-           </div> 
+           <div class="col-sm col-md-2 saveBtn">
+           <i class="far fa-save fa-2x" id=icon></i>  
            </div>`);
 }
-console.log(plannedHours);
+console.log(scheduledHours);
 
 //Check the hour of the current day to the hour represented in the HTML data-element to decide it's background color.
 
