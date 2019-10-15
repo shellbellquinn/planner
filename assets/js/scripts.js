1fee3a8ef881dfae4ce215c0a470a3397e89f325
Michelle Quinn <michelle.n.quinn@gmail.com>
2019-10-15 15:37:04 -0500

    psuedo code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,19 +4,21 @@ $("#currentDay").html(dateString);
 
 // //create html timeblocks with for loop starting at 9 am until 5pm//
 var plannedHours = [];
-    
-    for (var hour = 9; hour < 18; hour++) {
-      plannedHours.push(moment({hour}).format('h  a'));
-      $('.container').append(`<div class="row time-block"> 
+
+for (var hour = 9; hour < 18; hour++) {
+    plannedHours.push(moment({
+        hour
+    }).format('h  a'));
+    $('.container').append(`<div class="row time-block"> 
 
        <!--//hour column//-->
            <div class="col-sm col-md-2 hour"> 
-             <p id=hour>${moment({hour}).format('h  a')}</p>
+             <p class=dayHour>${moment({hour}).format('h  a')}</p>
            </div> 
       
        <!--//user input text area//-->
            <div class="col-sm col-md-8 d-flex description"> 
-             <textarea></textarea> 
+             <textArea class=textArea></textarea> 
            </div> 
       
        <!--//bootstrap saveBtn//-->
@@ -24,6 +26,13 @@ var plannedHours = [];
              <i class="far fa-save fa-2x"></i> 
            </div> 
            </div>`);
-    }
-  console.log( plannedHours );
+}
+console.log(plannedHours);
+
+//Check the hour of the current day to the hour represented in the HTML data-element to decide it's background color.
+
+//Check for local storage to set the text in text area
+
+//Save on click to local storage
+
 
